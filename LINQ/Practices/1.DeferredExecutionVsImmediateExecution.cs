741a5a04a629650d9e5af97ca2231d7a9a56a8a3
Nithyapriya Vellaisamy <nithyapriyav66@gmail.com>
2025-02-10 17:39:37 +0530

    Deffered execution vs immediate execution.

---
+++ LINQ/Practices/1.DeferredExecutionVsImmediateExecution.cs
@@ -0,0 +1,35 @@
+﻿namespace LINQ.Practices
+{
+    public class DeferredExecutionVsImmediateExecution
+    {
+        public DeferredExecutionVsImmediateExecution()
+        {
+            DeferredExecution();
+            ImmediateExecution();
+        }
+        public void DeferredExecution()
+        {
+            IEnumerable<int> numbers = Enumerable.Range(1, 10).Where(n => n % 2 == 0); // Query is not executed yet
+            numbers = numbers.Where(n => n % 4 == 0); // Query is not executed yet
+
+            foreach (var num in numbers) // Execution happens here
+            {
+                Console.WriteLine(num);
+            }
+        }
+        public void ImmediateExecution()
+        {
+            List<int> numbers = Enumerable.Range(1, 10).Where(n => n % 2 == 0).ToList(); // Query executes immediately
+            numbers = numbers.Where(n => n % 4 == 0).ToList();// Query executes immediately
+
+            foreach (var num in numbers) // Already materialized, no further execution
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
+
+// In Deffered execution recomputations(-)  low memory usage(+)
+//In Immediate execution memory usage(-) no recomputations(+)
+//note: A new list will be created Whenever you give ToList(). Aware of it. 
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace LINQ
+﻿using LINQ.Practices;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("LINQ Practice");
+            new DeferredExecutionVsImmediateExecution();
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
